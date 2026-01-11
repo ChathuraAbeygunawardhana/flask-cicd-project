356d90217b6ddf4246e6ca0ac329f899821534f3
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2026-01-11 20:56:44 +0530

    change commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! CI/CD Pipeline is Working!"
+    return "Hello World! CI/CD Pipeline is Working! 🙃"
 
 
 @app.route("/health")
